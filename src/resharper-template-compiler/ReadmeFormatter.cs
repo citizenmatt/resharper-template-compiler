3bb6ce5342c5aefae4bbce65392b3472b4f0602a
Matt Ellis <m.t.ellis@gmail.com>
2023-01-16 14:34:13 +0000

    Format table cleanly

--- src/resharper-template-compiler/ReadmeFormatter.cs
+++ src/resharper-template-compiler/ReadmeFormatter.cs
@@ -53,10 +53,10 @@ namespace CitizenMatt.ReSharper.TemplateCompiler
         private void FormatTemplates(IEnumerable<Template> templates)
         {
             foreach (var groupedTemplates in templates.GroupBy(t => t.Type))
-                FormatTemplates(groupedTemplates.Key, groupedTemplates);
+                FormatTemplates(groupedTemplates.Key, groupedTemplates.ToList());
         }
 
-        private void FormatTemplates(TemplateType templateType, IEnumerable<Template> templates)
+        private void FormatTemplates(TemplateType templateType, IReadOnlyCollection<Template> templates)
         {
             switch(templateType)
             {
@@ -74,22 +74,27 @@ namespace CitizenMatt.ReSharper.TemplateCompiler
             }
         }
 
-        private void FormatLiveTemplates(IEnumerable<Template> templates)
+        private void FormatLiveTemplates(IReadOnlyCollection<Template> templates)
         {
             writer.WriteLine();
             writer.WriteLine("### Live Templates");
             writer.WriteLine();
-            writer.WriteLine("Shortcut | Description");
-            writer.WriteLine("---------|------------");
-            foreach (var template in templates.OrderBy(t => t.Shortcut))
-                writer.WriteLine("[{0}]({1}) | {2}", template.Shortcut, GetRelativePath(template.InputFile), template.Description);
+
+            var sortedTemplates = templates.OrderBy(t => t.Shortcut).ToList();
+            var columns = new[]
+            {
+                new string[sortedTemplates.Count],
+                new string[sortedTemplates.Count]
+            };
+            for (var i = 0; i < sortedTemplates.Count; i++)
+            {
+                var template = sortedTemplates[i];
+                columns[0][i] = $"[{template.Shortcut}]({GetRelativePath(template.InputFile)})";
+                columns[1][i] = template.Description;
+            }
+            FormatTable(new[] { "Shortcut", "Description" }, columns);
 
             writer.WriteLine();
-        }
-
-        private void FormatSurroundTemplates(IEnumerable<Template> templates)
-        {
-            throw new NotImplementedException("Surround templates not supported in README generation");
         }
 
         private void FormatFileTemplates(IEnumerable<Template> templates)
@@ -97,12 +102,52 @@ namespace CitizenMatt.ReSharper.TemplateCompiler
             writer.WriteLine();
             writer.WriteLine("### File Templates");
             writer.WriteLine();
-            writer.WriteLine("Description |");
-            writer.WriteLine("------------|");
-            foreach (var template in templates.OrderBy(t => t.Description))
-                writer.WriteLine("[{0}]({1}) |", template.Description, GetRelativePath(template.InputFile));
+
+            var sortedTemplates = templates.OrderBy(t => t.Description).ToList();
+            var columns = new[] { new string[sortedTemplates.Count] };
+            for (var i = 0; i < sortedTemplates.Count; i++)
+            {
+                var template = sortedTemplates[i];
+                columns[0][i] = $"[{template.Description}]({GetRelativePath(template.InputFile)})";
+            }
+            FormatTable(new[] { "Description" }, columns);
 
             writer.WriteLine();
+        }
+
+        private void FormatTable(IReadOnlyList<string> headers, IReadOnlyList<string[]> columns)
+        {
+            var columnWidths = new int[headers.Count];
+            for (var i = 0; i < columns.Count; i++)
+            {
+                var width = headers[i].Length;
+                foreach (var row in columns[i])
+                    width = Math.Max(width, row.Length);
+                columnWidths[i] = width;
+            }
+
+            writer.Write("|");
+            for (var i = 0; i < headers.Count; i++)
+                writer.Write(" " + headers[i].PadRight(columnWidths[i]) + " |");
+            writer.WriteLine();
+
+            writer.Write("|");
+            for (var i = 0; i < headers.Count; i++)
+                writer.Write(new string('-', columnWidths[i] + 2) + "|");
+            writer.WriteLine();
+
+            for (var i = 0; i < columns[0].Length; i++)
+            {
+                writer.Write("|");
+                for (var j = 0; j < columns.Count; j++)
+                    writer.Write(" " + columns[j][i].PadRight(columnWidths[j]) + " |");
+                writer.WriteLine();
+            }
+        }
+
+        private void FormatSurroundTemplates(IEnumerable<Template> templates)
+        {
+            throw new NotImplementedException("Surround templates not supported in README generation");
         }
 
         private string GetRelativePath(string fullPath)
